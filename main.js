7ff88b56f436d2d7c8d5d00fe774ed12de541012
thenups <nn.mathur@gmail.com>
2018-07-01 14:05:50 -0700

    prelim js dict function

--- main.js
+++ main.js
@@ -3,3 +3,10 @@ var dict = {
     'tandoori': 2,
     'masala': 3
 };
+
+function whichSite(psw) {
+
+    while (psw !== password) {
+        alert("Incorrect Password");
+        return promptPass();
+}
